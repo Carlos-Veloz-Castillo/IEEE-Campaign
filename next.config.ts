6098716203f11e90d49daf837e9400e3944ff1f8
Carlos Veloz <cv3108@mavs.uta.edu>
2026-04-08 23:16:24 -0500

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/ieee-campaign",
+  basePath: "/IEEE-Campaign",
   images: {
     unoptimized: true,
   },
